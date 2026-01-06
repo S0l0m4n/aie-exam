85d2c2501739a48a579fbfdd099a0ed5463e7be6
S0l0m4n <sam@stickerbrush>
2026-01-06 07:30:01 +0200

    Start fetch_urls: Uses asyncio/httpx libraries, batch URLs

---
+++ aie-exam/fetch_urls.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+# Given a list of URLs, fetch them concurrently with a max concurrency limit and
+# collect the successful JSON results.
+
+import asyncio
+import httpx
+
+URL_BASE = "https://jsonplaceholder.typicode.com/posts/"
+OTHER_URL = "https://docs.python.org/3.10/library/stdtypes.html#text-sequence-type-str"
+
+URLS = [
+        URL_BASE + "1",
+        OTHER_URL,
+        URL_BASE + "2",
+        URL_BASE + "3",
+        URL_BASE + "4",
+        URL_BASE + "5",
+]
+
+MAX_CONCURRENT = 3
+
+async def fetch_url(client, url):
+    """
+    Args:
+        client:     httpx.AsyncClient
+        url:        URL to fetch
+    """
+    try:
+        resp = await client.get(url)
+        print(f"✅ {url}: {resp.status_code}")
+    except httpx.ConnectTimeout:
+        print(f"❌ {url}: Failed to connect, server too slow")
+    except httpx.HTTPError as e:
+        print(f"❌ {url}: Error occured, {e}")
+
+async def fetch_all():
+    # use AsyncClient to keep the connection open efficiently
+    async with httpx.AsyncClient(timeout=6) as client:
+        for i in range(0, len(URLS), MAX_CONCURRENT):
+            batch = URLS[i : i + MAX_CONCURRENT]
+            tasks = [ fetch_url(client, url) for url in batch ]
+            print(f"Processing batch: {batch}")
+            await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    asyncio.run(fetch_all())
+
+"""
+response = requests.head(OTHER_URL)
+if "json" in response.headers['Content-Type']:
+    response = requests.get(OTHER_URL)
+    print("YES")
+else:
+    print("NO")
+"""
