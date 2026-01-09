c58dcab72bcca7a9743e2188f0c08c999036ef01
S0l0m4n <sam@stickerbrush>
2026-01-09 14:31:52 +0200

    openai-chat: Add chat parser, list of input messages

--- aie-exam/openai-chat.py
+++ aie-exam/openai-chat.py
@@ -4,6 +4,7 @@
 # Use the Requests library to make the requests.
 
 import os
+import pdb
 import requests
 
 OPENAI_URL = "https://api.openai.com/v1/responses"
@@ -16,9 +17,30 @@ headers = {
 body = {
         "model": "gpt-4o-mini",
         "temperature": 0.15,
-        "input": "Test",
+        "max_output_tokens": 150,
 }
 
-response = requests.post(OPENAI_URL, headers=headers, json=body)
+def chat_parser(response:dict) -> str:
+    answers = []
+    for item in response.get("output", []):
+        if item.get("type") == "message" and item.get("role") == "assistant":
+            for message in item.get("content", []):
+                if message.get("type") == "output_text":
+                    text = message.get("text", "")
+                    answers.append(text)
+    return "".join(answers)
 
-print(response, response.text)
+def post_messages(messages):
+    req_body = body.copy()
+    req_body['input'] = messages
+    response = requests.post(OPENAI_URL, headers=headers, json=req_body)
+    response.raise_for_status()
+    return chat_parser(response.json())
+
+if __name__ == '__main__':
+    messages = [
+            {"role": "user", "content": "Hello, can you help me today?"},
+            {"role": "user", "content": "What is the currency of Lebanon?"},
+    ]
+    answers = post_messages(messages)
+    print(answers)
